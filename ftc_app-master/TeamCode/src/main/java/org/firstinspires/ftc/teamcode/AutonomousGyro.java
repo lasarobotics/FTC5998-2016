21b8a0e827d2dbe5529d4cfce925da34c31de242
Ethan Schaffer <etanbet@gmail.com>
2016-12-06 18:35:11 -0600

    Gyro Work
    New Gyro based files.
    TODO: Write a turnToPos() state that turns the robot to a specific
    state.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousGyro.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousGyro.java
@@ -13,18 +13,26 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  * Created by Ethan Schaffer on 9/24/2016.
  */
 @Autonomous(name = "MR: Gyro", group = "Autonomous")
-@Disabled   // comment out or remove this line to enable this opmode
+//@Disabled   // comment out or remove this line to enable this opmode
 public class AutonomousGyro extends LinearOpMode {
     ModernRoboticsI2cGyro gyroSensor;
     @Override
     public void runOpMode() throws InterruptedException {
         gyroSensor = hardwareMap.get(ModernRoboticsI2cGyro.class, "g");
         gyroSensor.calibrate();
-        while(gyroSensor.isCalibrating()){
-            telemetry.addData("Gyro Reading", "Calibrating...");
+        int timer = 0;
+        while(gyroSensor.isCalibrating()) {
+            timer++;
+            if (timer % 30 < 10) {
+                telemetry.addData("Gyro", " is Calibrating.");
+            } else if (timer % 30 < 20){
+                telemetry.addData("Gyro", " is Calibrating..");
+            } else {
+                telemetry.addData("Gyro", " is Calibrating...");
+            }
             telemetry.update();
         }
-        telemetry.addData("Gyro Reading", "CALIBRATED");
+        telemetry.addData("Gyro", "is Calibrated");
         telemetry.update();
         waitForStart();
 
