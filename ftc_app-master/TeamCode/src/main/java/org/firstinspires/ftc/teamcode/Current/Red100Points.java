f796544a93f6ca5961b3df30e21ac91aefb7d795
Ethan Schaffer <etanbet@gmail.com>
2017-02-06 21:01:36 -0600

    Add New Routes
    And Fix Red100 to account for collisions.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Current/Red100Points.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Current/Red100Points.java
@@ -21,32 +21,59 @@ public class Red100Points extends LinearOpMode {
         robot.Move(65, 1.0);
         robot.EnableShot(250, 1);
         robot.StopShooter();
-        robot.DiagonalForwardsLeft(25, 1);
+        boolean didCrash = robot.DiagonalForwardsLeft(25, 1);
         sleep(100);
         robot.AlignToWithin(.5, .05);
-        robot.LineSearch(2, .15);
-        sleep(100);
-        robot.LineSearch(2, - .05);
+        if(!didCrash) {
+            robot.LineSearch(2, .15);
+            sleep(100);
+            robot.LineSearch(2, -.05);
+        } else {
+            robot.LineSearch(2, -.15);
+            sleep(100);
+            robot.LineSearch(2, .05);
+        }
         robot.StrafeToWall(10, .08);
         robot.StrafeToWall(9, .07);
         robot.PressBeacon(Robot.team.Red);
 
         robot.StrafeFromWall(13, 1.0);
         robot.AlignToWithin(1, .05);
-        robot.Move(130, -1.0);
+        if (!didCrash){
+            robot.Move(130, -1.0);
+        } else {
+            robot.Move(130, 1.0);
+        }
 
         robot.AlignToWithin(1, .05);
 
-        robot.LineSearch(2, -.15);
+        if(!didCrash){
+            robot.LineSearch(2, -.15);
+        } else {
+            robot.LineSearch(2, .15);
+        }
         sleep(100);
         robot.StrafeToWall(10, .08);
         robot.StrafeToWall(9, .07);
-        robot.LineSearch(2, .05);
+        if(!didCrash){
+            robot.LineSearch(2, .05);
+        } else {
+            robot.LineSearch(2, -.05);
+        }
+
         robot.PressBeacon(Robot.team.Red);
         robot.StrafeFromWall(25, .75);
-        robot.AlignToWithin(1.5, .05);
-        robot.Move(115, -1.0);
-        robot.Move(25, -0.25);
+        if(!didCrash){
+            robot.AlignToWithin(1.5, .05);
+            robot.Move(115, -1.0);
+            robot.Move(25, -0.25);
+        } else {
+            robot.TurnRightEnc(35, 0.50);
+            robot.Move(15, 1.0);
+            robot.Move(15, -.75);
+            robot.Move(15, 0.50);
+            robot.Move(15, -0.25);
+        }
 
     }
 }
